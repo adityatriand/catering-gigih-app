d86876e3a8a875625f4d574795c5ecb186ebf90f
adityatriand <adityatriananda0@gmail.com>
2022-04-20 08:56:36 +0700

    add validation for name in Item Model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -14,6 +14,34 @@ RSpec.describe Item, type: :model do
     it 'is valid with a name, a description, and a price' do
       expect(item).to be_valid
     end
+
+  end
+
+  context 'validation for name field' do
+    
+    it 'is invalid without a name' do
+      item.name = nil
+      item.valid?
+      expect(item.errors[:name]).to include("can't be blank")
+    end
+
+    it "is invalid with a duplicate name" do 
+      item1 = Item.create(
+        name: "Nasi Uduk",
+        description: "Betawi style steamed rice cooked in coconut milk. Delicious!",
+        price: 10000.0
+      )
+      
+      item2 = Item.new(
+        name: "Nasi Uduk",
+        description: "Just with a different description.",
+        price: 10000.0
+      )
+  
+      item2.valid?
+      
+      expect(item2.errors[:name]).to include("has already been taken")
+    end
     
   end
 
