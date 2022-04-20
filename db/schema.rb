4bd41bdf3c1d97874c071d72388396a1cff99a12
adityatriand <adityatriananda0@gmail.com>
2022-04-20 09:15:12 +0700

    add validation for ItemCategory Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_014545) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_020938) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "item_categories", force: :cascade do |t|
+    t.integer "item_id", null: false
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_item_categories_on_category_id"
+    t.index ["item_id"], name: "index_item_categories_on_item_id"
   end
 
   create_table "items", force: :cascade do |t|
@@ -25,4 +34,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_04_20_014545) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "item_categories", "categories"
+  add_foreign_key "item_categories", "items"
 end
---
+++ spec/models/item_category_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.describe ItemCategory, type: :model do
+  let(:category){
+    Category.new(
+      name: 'Main Dishes'
+    )
+  }
+  let(:item){
+    Item.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0
+    )
+  }
+  subject(:item_category){
+    ItemCategory.new(
+      item: item,
+      category: category
+    )
+  }
+
+  context 'validation for all field' do
+    
+    it 'is valid with item and category' do
+      expect(item_category).to be_valid
+    end
+
+  end
+
+end
