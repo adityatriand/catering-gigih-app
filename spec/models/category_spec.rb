96cc671092f7ba903deb147515376d0126a29899
adityatriand <adityatriananda0@gmail.com>
2022-04-20 08:42:23 +0700

    add validation for Category, is valid with a name

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Category, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'validation for all field' do
+    
+    it 'is valid with a name' do
+      
+      food = Category.new(
+        name: 'Makanan Utama'
+      )
+  
+      expect(food).to be_valid
+    end
+    
+  end
 end
