6f5620fd4a866ac80967f0194a5d145fb932b60f
adityatriand <adityatriananda0@gmail.com>
2022-04-20 08:47:55 +0700

    generate scaffold Item

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_04_20_013639) do
+ActiveRecord::Schema[7.0].define(version: 2022_04_20_014545) do
   create_table "categories", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
