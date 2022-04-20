94a5ac667c1ff71ea10bfa83f70490e5d348fb62
adityatriand <adityatriananda0@gmail.com>
2022-04-20 08:52:29 +0700

    add validation for all field in Item model

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  subject(:item) {
+    Item.new(
+      name: 'Nasi Uduk',
+      description: 'Betawi style steamed rice cooked in coconut milk. Delicious!',
+      price: 15000.0
+    )
+  }
+
+  context 'validation for all field' do
+    
+    it 'is valid with a name, a description, and a price' do
+      expect(item).to be_valid
+    end
+    
+  end
+
+
+
 end
