fdcfbbee26f9261b6b69dd2b373f2f1e2f072cd0
adityatriand <adityatriananda0@gmail.com>
2022-04-22 11:27:48 +0700

    fix category page

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,9 @@
 class CategoriesController < ApplicationController
-  before_action :set_category, only: %i[ show edit update destroy ]
+  before_action :set_category, only: %i[edit update destroy ]
 
   # GET /categories or /categories.json
   def index
     @categories = Category.all
-  end
-
-  # GET /categories/1 or /categories/1.json
-  def show
   end
 
   # GET /categories/new
@@ -25,11 +21,9 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       if @category.save
-        format.html { redirect_to category_url(@category), notice: "Category was successfully created." }
-        format.json { render :show, status: :created, location: @category }
+        format.html { redirect_to categories_url, notice: "Category was successfully created." }
       else
         format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -38,11 +32,9 @@ class CategoriesController < ApplicationController
   def update
     respond_to do |format|
       if @category.update(category_params)
-        format.html { redirect_to category_url(@category), notice: "Category was successfully updated." }
-        format.json { render :show, status: :ok, location: @category }
+        format.html { redirect_to categories_url, notice: "Category was successfully updated." }
       else
         format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @category.errors, status: :unprocessable_entity }
       end
     end
   end
@@ -53,7 +45,6 @@ class CategoriesController < ApplicationController
 
     respond_to do |format|
       format.html { redirect_to categories_url, notice: "Category was successfully destroyed." }
-      format.json { head :no_content }
     end
   end
 
